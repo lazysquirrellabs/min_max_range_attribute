d7dd32cfff719aa03d0062b6aa875c406375fafd
Matheus Amazonas <matheus.amazonas@gmail.com>
2023-10-30 16:30:35 +0100

    ✨ Add support for Vector2

--- Assets/Scripts/MinMaxRangeDrawer.cs
+++ Assets/Scripts/MinMaxRangeDrawer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,13 +9,27 @@ namespace SneakySquirrelLabs.MinMaxRangeAttribute
 	{
 		#region Fields
 
-		private const float Spacing = 5f;
+		private const float HorizontalSpacing = 5f;
 		private const float OffsetX = -17f;
 		private const float OffsetY = 3f;
+		private static readonly float VerticalSpacing = EditorGUIUtility.standardVerticalSpacing;
+
+		private static GUIStyle LabelStyleField;
+
+		#endregion
+
+		#region Properties
+
+		private static GUIStyle LabelStyle => LabelStyleField ??= new GUIStyle(GUI.skin.label);
 
 		#endregion
 		
 		#region Update
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			return base.GetPropertyHeight(property, label) * 2;
+		}
 
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
@@ -30,70 +45,91 @@ namespace SneakySquirrelLabs.MinMaxRangeAttribute
 			if (property.propertyType == SerializedPropertyType.Vector2Int)
 			{
 				var value = property.vector2IntValue;
-				var x = (float) value.x;
-				var y = (float) value.y;
-				var consumedX = 0f;
-				
-				// Field name
-				consumedX += DrawFieldName(position, property);
-				
-				// Min label
-				var style = new GUIStyle(GUI.skin.label);
-				var minLabel = new GUIContent($"{min:F0}");
-				consumedX += DrawLabel(position, consumedX, minLabel, style);
-				consumedX += Spacing; // Add spacing between min label and slider
-				
-				// Slider
-				var maxLabel = new GUIContent($"{max:F0}");
-				var maxLabelWidth = style.CalcSize(maxLabel).x; // We need to know the max label's size
-				var sliderWidth = position.width - consumedX - maxLabelWidth - Spacing;
-				var sliderPosition = new Rect(position) { x = position.x + consumedX, width = sliderWidth };
-				EditorGUI.MinMaxSlider(sliderPosition, ref x, ref y, min, max);
-				consumedX += sliderWidth + Spacing;
-				
-				// Save value
+				var x = (float)value.x;
+				var y = (float)value.y;
+				DrawSlider(position, property, min, max, ref x, ref y, BuildIntLabel);
 				value.x = (int)x;
 				value.y = (int)y;
 				property.vector2IntValue = value;
+			} 
+			else if (property.propertyType == SerializedPropertyType.Vector2)
+			{
+				var value = property.vector2Value;
+				var x = value.x;
+				var y = value.y;
+				DrawSlider(position, property, min, max, ref x, ref y, BuildFloatLabel);
+				value.x = x;
+				value.y = y;
+				property.vector2Value = value;
+			}
+
+			static void DrawSlider(Rect position, SerializedProperty property, float min, float max, ref float x, 
+				ref float y, Func<float, GUIContent> buildLabel)
+			{
+				var consumedX = 0f;
+				var firstLineRect = new Rect(position) { height = position.height / 2 - VerticalSpacing};
+				
+				// Field name
+				consumedX += DrawFieldName(firstLineRect, property);
+				
+				// Min label
+				var minLabel = buildLabel(min);
+				consumedX += DrawLabel(firstLineRect, consumedX, minLabel);
+				consumedX += HorizontalSpacing; // Add spacing between min label and slider
+				
+				// Slider
+				var maxLabel = buildLabel(max);
+				var maxLabelWidth = LabelStyle.CalcSize(maxLabel).x; // We need to know the max label's size
+				var sliderWidth = firstLineRect.width - consumedX - maxLabelWidth - HorizontalSpacing;
+				var sliderPosition = new Rect(firstLineRect) { x = firstLineRect.x + consumedX, width = sliderWidth };
+				EditorGUI.MinMaxSlider(sliderPosition, ref x, ref y, min, max);
+				// consumedX += DrawSlider(position, maxLabelWidth, min, max, ref x, ref y);
+				consumedX += sliderWidth + HorizontalSpacing;
 				
 				// Max label
-				DrawLabel(position, consumedX, maxLabel, style);
+				DrawLabel(firstLineRect, consumedX, maxLabel);
 				
 				// Value labels
-				var valuesY = position.y + sliderPosition.height + EditorGUIUtility.standardVerticalSpacing;
-				style.alignment = TextAnchor.MiddleRight;
+				var secondLineRect = new Rect(position) { y = position.y, height = firstLineRect.height};
+				var valuesY = secondLineRect.y + sliderPosition.height + EditorGUIUtility.standardVerticalSpacing;
+				LabelStyle.alignment = TextAnchor.MiddleRight;
 				// X label
 				var labelsPosition = new Rect(sliderPosition) { y = valuesY };
-				DrawValueLabel(labelsPosition, value.x, min, max, style, OffsetX);
+				DrawValueLabel(labelsPosition, x, min, max, OffsetX, buildLabel);
 				// Label
-				style.alignment = TextAnchor.MiddleLeft;
-				DrawValueLabel(labelsPosition, value.y, min, max, style, OffsetY);
+				LabelStyle.alignment = TextAnchor.MiddleLeft;
+				DrawValueLabel(labelsPosition, y, min, max, OffsetY, buildLabel);
+				
+				static float DrawFieldName(Rect position, SerializedProperty property)
+				{
+					var labelPosition = new Rect(position) { width = EditorGUIUtility.labelWidth };
+					EditorGUI.LabelField(labelPosition, property.displayName);
+					return labelPosition.width;
+				}
+
+				static float DrawLabel(Rect position, float xOffset, GUIContent label)
+				{
+					var size = LabelStyle.CalcSize(label);
+					var minLabelPosition = new Rect(position) { x = position.x + xOffset, width = size.x};
+					EditorGUI.LabelField(minLabelPosition, label, LabelStyle);
+					return size.x;
+				}
+
+				static void DrawValueLabel(Rect position, float value, float min, float max,
+					float offset, Func<float, GUIContent> buildLabel)
+				{
+					var label = buildLabel(value);
+					var labelSize = LabelStyle.CalcSize(label);
+					var relativePosition = (value - min) / (max - min);
+					var x = position.x + relativePosition * position.width + offset;
+					var labelPosition = new Rect(position) { x = x, width = labelSize.x };
+					EditorGUI.LabelField(labelPosition, label, LabelStyle);
+				}
 			}
 
-			static float DrawFieldName(Rect position, SerializedProperty property)
-			{
-				var labelPosition = new Rect(position) { width = EditorGUIUtility.labelWidth };
-				EditorGUI.LabelField(labelPosition, property.displayName);
-				return labelPosition.width;
-			}
-
-			static float DrawLabel(Rect position, float xOffset, GUIContent label, GUIStyle style)
-			{
-				var size = style.CalcSize(label);
-				var minLabelPosition = new Rect(position) { x = position.x + xOffset, width = size.x};
-				EditorGUI.LabelField(minLabelPosition, label, style);
-				return size.x;
-			}
+			static GUIContent BuildIntLabel(float value) => new($"{value:F0}");
 			
-			static void DrawValueLabel(Rect position, int value, float min, float max, GUIStyle style, float offset)
-			{
-				var label = new GUIContent(value.ToString());
-				var labelSize = style.CalcSize(label);
-				var relativePosition = (value - min) / (max - min);
-				var x = position.x + relativePosition * position.width + offset;
-				var labelPosition = new Rect(position) { x = x, width = labelSize.x};
-				EditorGUI.LabelField(labelPosition, label, style);
-			}
+			static GUIContent BuildFloatLabel(float value) => new($"{value:F1}");
 		}
 
 		#endregion
