ce57422ebdd88c6f2d47e82a295f4db21a15e286
Matheus Amazonas <matheus.amazonas@gmail.com>
2023-10-31 08:58:03 +0100

    📝 Comment attribute code

--- Assets/Libraries/MinMaxRangeAttribute/MinMaxRangeAttribute.cs
+++ Assets/Libraries/MinMaxRangeAttribute/MinMaxRangeAttribute.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace SneakySquirrelLabs.MinMaxRangeAttribute
 {
+	/// <summary>
+	/// An attribute that simplifies defining bounded ranges (ranges with a minimum and maximum) on the inspector.
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Field)]
 	public class MinMaxRangeAttribute : PropertyAttribute
 	{
@@ -16,12 +19,24 @@ namespace SneakySquirrelLabs.MinMaxRangeAttribute
 
 		#region Setup
 
+		/// <summary>
+		/// A bounded range for integers.
+		/// </summary>
+		/// <param name="min">The minimum acceptable value.</param>
+		/// <param name="max">The maximum acceptable value.</param>
 		public MinMaxRangeAttribute(int min, int max)
 		{
 			Min = min;
 			Max = max;
 		}
 		
+		/// <summary>
+		/// A bounded range for floats.
+		/// </summary>
+		/// <param name="min">The minimum acceptable value.</param>
+		/// <param name="max">The maximum acceptable value.</param>
+		/// <param name="decimals">How many decimals the inspector labels should display. Values must be in the [0,3]
+		/// range. Default is 1.</param>
 		public MinMaxRangeAttribute(float min, float max, uint decimals = 1)
 		{
 			Min = min;
